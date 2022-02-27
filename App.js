5bae93cb67d664fba505701eb2d5c318d2d421ed
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2022-02-28 00:48:12 +0530

    Backdrop added and font fixes

--- App.js
+++ App.js
@@ -2,7 +2,6 @@
 import React,{useState,useEffect} from 'react';
 import Rating from './Rating';
 const { width, height } = Dimensions.get('window');
-
 import {
   StatusBar,
   Text,
@@ -13,13 +12,69 @@ import {
   Dimensions,
   Animated,
   TouchableOpacity,
+  Linking,
   Platform,
 } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const SPACING = 10;
-const ITEM_SIZE = Platform.OS === 'ios' ? width * 0.72 : width * 0.74;
+const ITEM_SIZE = Platform.OS === 'ios' ? width * 0.72 : width * 0.90;
 const EMPTY_ITEM_SIZE = (width - ITEM_SIZE) / 2;
 const BACKDROP_HEIGHT = height * 0.65;
+
+const Backdrop = ({ movies, scrollX }) => {
+  return (
+    <View style={{ height: BACKDROP_HEIGHT, width, position: 'absolute' }}>
+      <FlatList
+        data={movies.reverse()}
+        keyExtractor={(item) => item.mal_id + '-backdrop'}
+        removeClippedSubviews={false}
+        contentContainerStyle={{ width, height: BACKDROP_HEIGHT }}
+        renderItem={({ item, index }) => {
+          if (!item.images.jpg.image_url) {
+            return null;
+          }
+          const translateX = scrollX.interpolate({
+            inputRange: [(index - 1) * ITEM_SIZE, (index ) * ITEM_SIZE],
+            outputRange: [0, width],
+            // extrapolate:'clamp'
+          });
+          return (
+            <Animated.View
+              removeClippedSubviews={false}
+              style={{
+                position: 'absolute',
+                width: translateX,
+                height,
+                overflow: 'hidden',
+              }}
+            >
+              <Image
+                source={{ uri: item.images.jpg.image_url }}
+                style={{
+                  width,
+                  height: BACKDROP_HEIGHT,
+                  position: 'absolute',
+                }}
+              />
+            </Animated.View>
+          );
+        }}
+      />
+      <LinearGradient
+        colors={['rgba(0, 0, 0, 0)', 'white']}
+        style={{
+          height: BACKDROP_HEIGHT,
+          width,
+          position: 'absolute',
+          bottom: 0,
+        }}
+      />
+    </View>
+  );
+};
+
+
 export default function App() {
   const [data,setData] = useState([]);
   const scrollX = React.useRef(new Animated.Value(0)).current;
@@ -30,6 +85,8 @@ export default function App() {
   },[])
   return (
     <View style={styles.container}>
+      <StatusBar hidden />
+      <Backdrop movies={data} scrollX={scrollX} />
        <Animated.FlatList
         showsHorizontalScrollIndicator={false}
         data={data}
@@ -52,9 +109,9 @@ export default function App() {
           }
 
           const inputRange = [
-            (index - 2) * ITEM_SIZE,
-            (index - 1) * ITEM_SIZE,
-            index * ITEM_SIZE,
+            (index -3) * ITEM_SIZE,
+            (index) * ITEM_SIZE,
+            (index) * ITEM_SIZE,
           ];
 
           const translateY = scrollX.interpolate({
@@ -64,7 +121,8 @@ export default function App() {
           });
 
           return (
-            <View style={{ width: ITEM_SIZE }}>
+       
+        <View style={{ width: ITEM_SIZE ,margin:0 }} onPress={() => Linking.openURL('http://google.com')} >
               <Animated.View
                 style={{
                   marginHorizontal: SPACING,
@@ -72,11 +130,10 @@ export default function App() {
                   alignItems: 'center',
                   transform: [{ translateY }],
                   backgroundColor: 'white',
-                  borderRadius: 34,
                 }}
               >
                 <Image
-                  source={{ uri: item.images.jpg.image_url}}
+                  source={{ uri: item.images.webp.image_url}}
                   style={styles.posterImage}
                 />
                 <Text style={{ fontSize: 24 }} numberOfLines={1}>
@@ -91,21 +148,6 @@ export default function App() {
           );
         }}
       />
-      {/* {data.map((item) =>{
-        return(
-          <View style={styles.card}>
-            <Image
-          source={{
-            uri: item.images.jpg.image_url,
-          }}
-          style={styles.posterImage}
-        />
-            <Text style={styles.title}>{item.title}</Text>
-            <Rating rating={item.score} />
-          </View>
-        );
-      })} */}
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -115,12 +157,6 @@ const styles = StyleSheet.create({
     fontFamily:'sans-serif',
     flex: 1,
     backgroundColor: '#fff',
-  },
-  carousel: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
   },
   posterImage: {
     width: '100%',
